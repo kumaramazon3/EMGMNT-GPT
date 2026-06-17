9fc2c42ae66af5cf8a27dd25dc3093a3d8bcc23d
afifauzi <afif_fauzi20@yahoo.com>
2026-06-17 11:47:09 +0800

    c2

--- Maruwa_Emgmt/obj/Debug/net8.0/Maruwa_Emgmt.AssemblyInfo.cs
+++ Maruwa_Emgmt/obj/Debug/net8.0/Maruwa_Emgmt.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Maruwa_Emgmt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c9b787f3eb10393fd563e99c99343a30b5e094c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Maruwa_Emgmt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Maruwa_Emgmt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
